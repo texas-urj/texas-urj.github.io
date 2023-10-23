ef11c1d0861a209f51efa39cda39ee2ba954016b
Meghna Vergis <meghnavergis@utexas.edu>
2023-10-23 12:20:33 -0500

    Added line to allow slash next.config.json

--- texas-urj/next.config.js
+++ texas-urj/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: true,
     output: 'export',
-}
-
-module.exports = nextConfig
+    }
+    module.exports = nextConfig
