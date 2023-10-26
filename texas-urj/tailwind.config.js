46114d078d8200c16c31488080aa1fb549f6bb46
Meghna Vergis <meghnavergis@utexas.edu>
2023-10-26 12:12:35 -0500

    apply .js to app

--- texas-urj/tailwind.config.js
+++ texas-urj/tailwind.config.js
@@ -4,7 +4,7 @@ const config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
