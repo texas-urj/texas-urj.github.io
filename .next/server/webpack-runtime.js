d3ea2170a8b036adee5170ad27ed543306aff747
Meghna Vergis <meghnavergis@utexas.edu>
2023-10-27 13:26:10 -0500

    center text, change body color

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9ed8ba81deeccd8c")
+/******/ 		__webpack_require__.h = () => ("b7462958cdf17b94")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23f983aa8edf2c01"; }
+/******/ 		__webpack_require__.h = function() { return "d40953e1a9fdb292"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
