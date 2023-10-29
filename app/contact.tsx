38ad611b39af2e1e6f9d439f67437d08f25395b7
sravyag24 <148296043+sravyag24@users.noreply.github.com>
2023-10-28 19:54:53 -0500

    updated the contact page!

---
+++ app/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+
+export default function Contact() {
+  return (
+    <main>
+      <Contact Us> Sample Header </h1>
+      <h3>Sample text [instagram: @texasurj email: editor.in.chief@texasurj.info] Texas URJ</h3>
+    </main>
+  )
+}
