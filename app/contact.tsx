1dc0324ad3db97394c201f0e87997522d323cc80
sravyag24 <148296043+sravyag24@users.noreply.github.com>
2023-11-09 10:10:52 -0600

    tried something

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,11 +1,21 @@
 import React from 'react'; 
+import Navigation from '../components/navbar';
 
 
+<<<<<<< HEAD
+export default function Home() {
+=======
 export default function contact() {
+>>>>>>> e9fbe470232591f34baafc88b61a4945b6592290
   return (
     <main>
+<<<<<<< HEAD
+      <h1> Contact Us </h1>
+      <h3>instagram: @texasurj<br></br>email: editor.in.chief@texasurj.info </h3>
+=======
       <h1> Contact Us Sample Header </h1>
       <h3>Sample text [instagram: @texasurj email: editor.in.chief@texasurj.info] Texas URJ</h3>
+>>>>>>> e9fbe470232591f34baafc88b61a4945b6592290
     </main>
   )
 }
