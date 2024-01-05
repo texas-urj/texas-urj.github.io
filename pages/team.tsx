17b8f44a4b995e965f1482d5e25e91935efed6a8
Meghna Vergis <meghnavergis@utexas.edu>
2024-01-04 22:45:41 -0600

    updated team to have react

---
+++ pages/team.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Team = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      {/* Add your blogs page content here */}
+    </div>
+  );
+};
+
+export default Team;
