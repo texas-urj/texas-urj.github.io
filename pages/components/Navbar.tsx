9fb72931e812c195ca590a7a30ed29d14be779b1
Meghna Vergis <meghnavergis@utexas.edu>
2024-01-04 22:43:28 -0600

    updated navbar links created team page removed dd

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -11,33 +11,16 @@ export default function Heading() {
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="Pioneer" img="https://photos.app.goo.gl/nbxFUMaauMrBgiNz6" rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
         <Navbar.Link href="#" active>
           Home
         </Navbar.Link>
-        <Navbar.Link href="">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
+        <Navbar.Link href="/about">About</Navbar.Link>
+        <Navbar.Link href="/pioneer/home">Pioneer</Navbar.Link>
+        <Navbar.Link href="/team">Team</Navbar.Link>
+        <Navbar.Link href="/contact">Contact</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
