f3c615a98dc5541e117b488f5a748cefab78e623
Meghna Vergis <meghnavergis@utexas.edu>
2024-01-28 19:16:42 -0600

    Application form and pioneer home.

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -8,21 +8,21 @@ const Apply = () => {
       <Navigation></Navigation>
       <h1>Apply</h1>
       <section>
-      <p>The application for the Texas Undergraduate Research Journal (URJ) are currently closed. Feel free to read the information below and stay tuned for information on our next application cycle.</p>
-      {/* <p>Position appointments will be for the 2023-2024 academic year. The deadline for the application is promptly Friday September 8th, 2023 at 11:59PM. Many of our staff choose to return year after year. There are no explicit requirements to be a part of the URJ; students of all years, majors, and skill sets have joined the URJ and made significant contributions. Please see the descriptions below to determine which position(s) interest you most.</p>
-      <p>Meetings will be weekly 6-7 or 7-8pm on Tuesday evenings throughout the fall and spring semesters. Meeting attendance is mandatory and required to remain in good standing. Please make sure you can make time to attend these meetings.</p>
+      {/* <p>The application for the Texas Undergraduate Research Journal (URJ) are currently closed. Feel free to read the information below and stay tuned for information on our next application cycle.</p> */}
+      <p>Position appointments will be for the 2023-2024 academic year. The deadline to apply is promptly Monday, February 12th, 2024 at 11:59PM. No applications will be accepted beyond the specified date. Interview invites will be sent the week following the specified application deadline. There are no explicit requirements to be a part of the URJ; students of all years, majors, and skill sets have joined the URJ and made significant contributions. We are currently interviewing for Assistant Directors of Finance. </p>
+      <p>Meetings will be weekly 7-8pm on Tuesday evenings throughout the fall and spring semesters. Meeting attendance and meaningful contributions are required to remain in good standing. Please make sure you can make time to attend these meetings.</p>
       <p>Feel free to contact Sriya Gullapalli at <a className='underline hover:no-underline' href='mailto:editor.in.chief@texasurj.info'>editor.in.chief@texasurj.info</a> with any questions or concerns. We look forward to reading your application.</p>
       </section>
       
       <section className="flex justify-center items-center mx-10">
       <iframe
         title="Apply here:"
-        src="https://forms.gle/gYdzbH1WreDG6HPs8"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfRBmmaW6P5i5NcJDyW_xTw5_a4zg5BrtEjd-QAm7ZltCE7eA/viewform"
         width="80%"
         height="400"
         className="self-center mx-10 shadow-lg rounded-md mb-10">
         Loading...
-      </iframe> */}</section>
+      </iframe></section>
 
       <section className="w-80"> 
       <Accordion collapseAll>
