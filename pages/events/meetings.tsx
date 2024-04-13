3e5bff8b4fe75c42c0d8ae12eb5e830bf836df2d
Meghna Vergis <meghnavergis@utexas.edu>
2024-04-13 15:20:07 -0500

    added meeting slides

--- pages/events/meetings.tsx
+++ pages/events/meetings.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import Navigation from '../components/Navigation'
 
-const meetings = () => {
+const pageTemplate = () => {
   return (
-    <div>meetings</div>
+    <>
+    <Navigation name='Texas URJ'></Navigation>
+    <h1>Title</h1>
+    <p>General meetings are a great opportunity to work hard and form connections with other Texas URJ members! See our meeting slides to get a better understanding of what we do each week.</p>
+    <h2 className='mb-4 p-4'>Spring 2024 Meeting Slides</h2>
+    <div className="place-items-center grid grid-auto grid-cols-4">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT4Yiq9YJj5zwQEab2YYUzQLU8lcZnM0f1bNgc0vUs_1UCQk4TpmGpiYAF_7WSbPpF8fjMctt_McHkY/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vS20RgeeA89oOJigYOuUAQpFFMXcFerhMf77Wue5FGcdLjlmso7BxAxECPF3HPOpkbrYCw556k5ai6b/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>   
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSxTgLkdhDZBS5xCANOOmVOT7WB97JtGawE1QiQUmijVyXIbr8Q1W6_a1O2W_-tOio1CCwBVsdv6OF6/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRqCUhxTUSKUyWrG-OkIb1phumiKunHZd597xJZCdRz4WFyQlKXejBAyuZkrAiuo764E92wBIqiU_ra/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRlZxbd_8GBovYY0s6yhDi6XnqL8zMOzNbSu95LBfytSuFCpe40MFk3hH2LGhfMXaPvm08MZcWHIOYi/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTpFAiTwlyPLW5W9YOjypDbRSNFZwsuPb9qSvKsnHSp6lI7mv5Xoq8J7wl79xUDv2lpUiOrt6eIRRTX/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQdD_5wSOLwLr-vP1GNoDyQ6ktNUct-hKkJkU9toCic0TJLFZ7ro9CyiIjB904md2A-tFkJCeN8L1Vp/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQB7VN7jdDeuYyvwy7OnF-c0V_MW9I2bbItoogsDTXqCPk1h9hx6PmpkeEQM0zJKXx9m6TN67M8MNCv/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRXsUv0d3yTTYhHmi94LGb5bvf4h900ntM0yMHVNL_lTP9DZy6oR1eEZm6drHPXt6L0uce82ZskL6eq/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    </div>
+    <h2 className='mb-4 p-4'>Fall 2023 Meeting Slides</h2>
+    <div className="place-items-center grid grid-auto grid-cols-4">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQfX75NV3PIHictPd5QlH_lJ8-TMZ3RchLTlDXkVPx3472d1fUSZxIhV9q1OcHUToop4bj3x0oQrEeB/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT1h4GvS8Cg5L-DKLCtXGrs5WJURxuEcQoziJ4ZikHj_Mcy3IsOrEW_yoIMUPg2le6yrUDygw9Rzzes/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR1EgWZ56-Oev4ABqLmWAbviFIOraMTIgXzLCdV7ysn17dyFAVP8u2OZ7cJoAG6ilqkXrrviVZ8t3M5/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTywCL0eqeVSulJ2RsATtDdyRs9RYU0N4i67sKxDzPXvoBDeF_qy8QHss7T6ulw5B18bn-5ONuDGsDV/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSa2zE_wAQCy08DpiSKZGAdj24luo8sqRX9uJ7r0PNe0M93Y4xHFVio1hhMHB_v4xBJuNqL_ql6P7nf/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQcVZWvfJgBnBYqGcTUMbLBE-hJQFeh340sQaUhLe1kZxjfQUVsmxsK_6CGJ_ZzrJculv0BQMCxnVl1/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSWrbM2IVMj0ghf-QS4gxFsEEtBM15sLotnxgRyB_krGHkMtgDo3l_MlX4zB_fZC9OjU6RBSBcYlDYA/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT1KHO2GDz0nGHRBI9PfylQWhF7QE77vCYxRV74ygtHSJUuA7o8alVdnE4jF7qZexbIK3WWGtXhbQKN/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTExNJhMWvNdMNryUOIfYXLz12UlfJkBt4EOwfpaWSb2twll6G5LUouCqO9tAzsWgHGMYnx2HHa6rEx/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTiRH06F3aM8dovA6u-zO6CQaENppJRhh2z5gqfdCpFEXGE-oNrL4t6K3JrzET2HA_aS0grWdBXd_Dc/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTmvxZfOTHVJyF96OImBZIeHMVjxVDIlIWv1Fp3XrLTFVejrxiPdtTKGIJLrEGqAMwW22TeMSEv-_6v/embed?start=false&loop=false&delayms=3000" width="380" height="200" className='p-4'></iframe>
+    </div>
+    </>
   )
 }
 
-export default meetings
+export default pageTemplate
