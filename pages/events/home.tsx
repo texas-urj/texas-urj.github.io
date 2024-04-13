1cc27f513a6a7022dba3ea7fe2de89eceabb9aef
sravyag24 <6sravya.gullapalli@gmail.com>
2024-04-13 12:48:07 -0500

    added ptp thumbnail

--- pages/events/home.tsx
+++ pages/events/home.tsx
@@ -11,7 +11,8 @@ const EventsHome = () => {
       <section className="grid grid-cols-4 gap-6 pb-10 mb-10 sm:grid-cols-1">
       <ArticleCard thumbnail='https://i.imgur.com/WddsjhK.png' title='Socials' route='/events/socials'></ArticleCard>
       <ArticleCard thumbnail='https://i.imgur.com/k0G8wwN.jpg' title='General Meetings' route='/events/meetings'></ArticleCard>
-      <ArticleCard thumbnail='https://i.imgur.com/TgaKoS7.jpg' title='Networking Night' route='/events/networkingnight'></ArticleCard></section>
+      <ArticleCard thumbnail='https://i.imgur.com/TgaKoS7.jpg' title='Networking Night' route='/events/networkingnight'></ArticleCard>
+      <ArticleCard thumbnail='https://i.imgur.com/zYNtu0b.jpg' title='Pen to Publication' route='/events/ptp'></ArticleCard></section>
     </>
   );
 };
