21d0508881bba9066a85389be71fb62772d124f1
nichiriuu <nichiriu@utexas.edu>
2024-04-14 16:17:30 -0500

    altering again pen to pub

--- pages/events/penToPublication.tsx
+++ pages/events/penToPublication.tsx
@@ -8,10 +8,10 @@ const penToPublication = () => {
     <Navigation name='Texas URJ'></Navigation>
     <h1>Pen to Publication</h1>
     <h2 className='mb-4 p-4'>The Texas Undergraduate Research Journal organized a panel discussion focused on undergraduate and early graduate publications. Graduate students shared their insights and experiences with joining research groups on campus and getting involved with higher education research. </h2>
-    <div className="grid grid-cols-4 gap-2">
+    <div className="grid grid-cols-4 place-items-center gap-4">
     <img className='mx-4 my-10 border-solid border-2 border-wolfgray w-1/4 h-auto' src="https://i.imgur.com/lpL6ikW.jpg"></img>
     <p className = "">Pictured: Panelists Jamie Palmer, Andrea Nam, Hyo-Mi (Mia) Pak, and Kate Davis.</p>
-    <img className='mx-4 my-10 border-solid border-2 border-wolfgray w-1/4 h-auto' src="https://i.imgur.com/WddsjhK.png"></img>
+    <img className='mx-4 my-10 border-solid border-2 border-wolfgray w-1/4 h-auto' src="https://i.imgur.com/Q3GsWaI.png"></img>
     </div>
     
     </>
