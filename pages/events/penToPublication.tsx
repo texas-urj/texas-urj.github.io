dd51893a8499a4233cbd6b24c2ee6bfd718c1f92
nichiriuu <nichiriu@utexas.edu>
2024-04-14 15:57:34 -0500

    modified pictures on pen to publication

--- pages/events/penToPublication.tsx
+++ pages/events/penToPublication.tsx
@@ -8,11 +8,10 @@ const penToPublication = () => {
     <Navigation name='Texas URJ'></Navigation>
     <h1>Title</h1>
     <h2 className='mb-4 p-4'>The Texas Undergraduate Research Journal organized a panel discussion focused on undergraduate and early graduate publications. Graduate students shared their insights and experiences with joining research groups on campus and getting involved with higher education research. </h2>
-    <div className="grid grid-cols-4 place-items-center gap-4">
-    <img className='mx-4 my-10 border-solid border-2 border-wolfgray w-1/4 h-auto' src="https://imgur.com/R9at5Tc"></img>
+    <div className="grid grid-cols-4 gap-2">
+    <img className='mx-4 my-10 w-full h-auto col-span-3' src="https://i.imgur.com/lpL6ikW.jpg"></img>
     <p className = "">Pictured: Panelists Jamie Palmer, Andrea Nam, Hyo-Mi (Mia) Pak, and Kate Davis.</p>
-    <img className='mx-4 my-10 border-solid border-2 border-wolfgray w-1/4 h-auto' src="https://imgur.com/Q3GsWaI"></img>
-    <p className = "">Sample text/quote here.</p>
+    <img className='mx-4 my-10 w-1/4 h-auto col-start-4' src="https://i.imgur.com/Q3GsWaI.png"></img>
     </div>
     
     </>
