82ae5d8391bf14b3334af10333e2f6250e541d7a
Meghna Vergis <meghnavergis@utexas.edu>
2024-10-22 19:34:45 -0500

    Removed members from last year who did not return

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -17,12 +17,9 @@ const Staff = () => {
       <AccordionPanel><AccordionTitle className='text-center'>Executive Staff</AccordionTitle>
       <AccordionContent>
       <div className='flex flex-col items-center mb-4'>
-        <TeamCard title = "Editor-in-Chief" name="Sriya Gullapalli" headshot="https://i.imgur.com/mtd03bN.jpg" bio="Sriya is a fourth-year Neuroscience major who is actively involved in research in the FRI Biobricks stream and the Fonken Lab. Her hobbies include photography, traveling, and exploring Austin. Contact her at editor.in.chief@texasurj.info"> </TeamCard>
-        <TeamCard title = "Executive Director of Campaigns and Communications" name="Shreya Nallaparaju" 
-        headshot="https://i.imgur.com/L4Onqj0.jpg" bio="Shreya is a fourth-year Biology major whose current research interests lie in computational science and biotechnology. In her free time, she enjoys nature walks/hikes, reading dystopian novels, and trying new restaurants. Contact her at shreya.nallaparaju@utexas.edu"> </TeamCard>
-        <TeamCard title = "Executive Editor" name="Rachel Muralitharan" 
+        <TeamCard title = "Editor-in-Chief" name="Rachel Muralitharan" 
         headshot="https://i.imgur.com/WhnSnF5.jpg" bio="Rachel Muralitharan is a third-year computational biology major who researches in the Virtual Drug Screening Lab and as a part of the Herrera-Diestra Lab. In her free time, Rachel enjoys reading mystery novels and trying new recipes. Contact her at rachel.murali@utexas.edu"> </TeamCard>
-        <TeamCard title = "Financial Director" name="Meghna Vergis" 
+        <TeamCard title = "Media Director" name="Meghna Vergis" 
         headshot="https://i.imgur.com/v5xhlIt.jpg" bio="Meghna is a second-year Biology major interested in going into research as a career-either academic or industrial. In her free time, she enjoys listening to music, playing games, and taking walks around the UT campus. Contact her at meghnavergis@utexas.edu"> </TeamCard>
         <TeamCard title = "Financial Director" name="Andrew Kulkarni" 
         headshot="https://i.imgur.com/D8VX4zq.jpg" bio="Andrew is a second-year biology major. He is interested in the administrative side of healthcare and learning more about factors that impede effective patient care on a structural level. In his free time, he enjoys hiking, playing basketball, investing in stocks, and trying new restaurants in Austin. Contact him at andrewskulkarni@gmail.com"> </TeamCard>
@@ -32,10 +29,6 @@ const Staff = () => {
         <Accordion.Title>Assistant Editors</Accordion.Title>
         <Accordion.Content>
       <div className='grid grid-rows-2 gap-4 justify-center'>
-      <TeamCard title = "" name="Megan George" 
-        headshot="https://i.imgur.com/YGE37JM.jpg" bio="Megan is a fourth-year neuroscience major with a business minor. She involved as a undergraduate research assistant in the Beer Lab and Harris Lab. Outside of her commitments, she enjoys spending time with her cat and reading romance novels. "> </TeamCard>
-      <TeamCard title = "" name="Sraavya Chintalapati" 
-        headshot="https://i.imgur.com/P2cvJb4.jpg" bio="Sraavya is a fourth-year Anthropology major."> </TeamCard>
         <TeamCard title = "" name="Marco Keller" 
         headshot="https://i.imgur.com/D3Q4m9D.jpg" bio="Marco is a third-year Biology major who is actively involved in his FRI lab (Plant Pathways) and is doing research in the Johnson Lab. In his free time he plays chess, tennis, and rides trails on his bike."> </TeamCard>
         <TeamCard title = "" name="Alp Kaya" 
@@ -64,8 +57,6 @@ const Staff = () => {
         headshot="https://i.imgur.com/0C8zaAg.jpg" bio="Arnav is a second-year Biochemistry major with a certificate in Spanish for Medical Professions."> </TeamCard>
         <TeamCard title = "" name="Albina Hashmi" 
         headshot="https://i.imgur.com/8d0zW4W.jpg" bio="Albina is a second-year Public Health major with a minor in Government. She is interested in global health and Healthcare advocacy. In her free time she likes reading, baking and listening to music."> </TeamCard>
-        <TeamCard title = "" name="Isha Jha" 
-        headshot="https://i.imgur.com/RswiNZ9.jpg" bio="Isha is a fourth-year Neuroscience major."> </TeamCard>
         <TeamCard title = "" name="Sifallah Bouyadjera" 
         headshot="https://i.imgur.com/gmE9Ph1.jpg" bio="Sifallah is a second-year Neuroscience major."> </TeamCard>
         </div></Accordion.Content>
