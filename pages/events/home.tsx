55c58acf7e1d2f77e2920bc0cb7bac36770db2c9
Meghna Vergis <meghnavergis@utexas.edu>
2024-10-29 19:15:14 -0500

    added RSVP for FF

--- pages/events/home.tsx
+++ pages/events/home.tsx
@@ -8,6 +8,7 @@ const EventsHome = () => {
     <>
       <Navigation name='Texas URJ'></Navigation>      
       <h1 className='pt-10'>Events</h1>
+      <p>Our Finding Fellowships panel is coming up on Tuesday, November 5th, 2024 from 7pm-8pm in the WCP Legislative Assembly Room 2.302. Join us to learn about how to find fellowship opportunities! <a href='https://docs.google.com/forms/d/e/1FAIpQLSfueP8b4iUTbi4DqRD7F0U4RvqaKfsFl1PqwbPuE9fJ7nRjAA/viewform'className='underline' >RSVP here!</a></p>
       <section className="grid grid-cols-4 gap-6 pb-10 mb-10 sm:grid-cols-1">
       <ArticleCard thumbnail='https://i.imgur.com/WddsjhK.png' title='Socials' route='/events/socials'></ArticleCard>
       <ArticleCard thumbnail='https://i.imgur.com/k0G8wwN.jpg' title='General Meetings' route='/events/meetings'></ArticleCard>
