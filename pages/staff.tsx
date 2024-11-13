ac603b2b2160e90795d32b54111d71f38525ab28
Meghna Vergis <meghnavergis@utexas.edu>
2024-11-12 19:31:25 -0600

    Added people 1-6

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -37,16 +37,22 @@ const Staff = () => {
         <Accordion.Title>Assistant Editors</Accordion.Title>
         <Accordion.Content>
       <div className='grid grid-rows-2 gap-4 justify-center'>
-        <TeamCard title = "" name="Marco Keller" 
-        headshot="https://i.imgur.com/D3Q4m9D.jpg" bio="Marco is a third-year Biology major who is actively involved in his FRI lab (Plant Pathways) and is doing research in the Johnson Lab. In his free time he plays chess, tennis, and rides trails on his bike."> </TeamCard>
-        <TeamCard title = "" name="Keshv Srinivasan" 
-        headshot="https://i.imgur.com/p4kkugE.jpg" bio="Keshv is a third-year Biochemistry major whose research interests are in stroke therapies and brain imaging technology. In his free time, he enjoys going on hikes, running, and watching sports."> </TeamCard>
+        <TeamCard title = "" name="Ishan Arora" 
+        headshot="" bio="Ishan is a first-year business major on the premed track whose current research interests lie in oncology and medical imaging. In his spare time he enjoys classical literature, healthcare investments, and Chess."> </TeamCard>
+        <TeamCard title = "" name="Dhroov Pathare" 
+        headshot="" bio="Dhroov is a first-year dual-majoring in Neuroscience and Plan II Honors. His current research interests lie in nanotechnology and medical oncology. In his free time, he enjoys going to concerts, mountain biking, traveling, and exploring new places to hang out around Austin."> </TeamCard>
+        <TeamCard title = "" name="Yashica Sadam" 
+        headshot="https://i.imgur.com/VCnMOMM.jpg" bio="Yashica is a second-year Health & Society major who enjoys reading, listening to music, and playing with her dog. She is currently actively involved in Partners in Health Engage and Texas Brain Exercise Initiative. "> </TeamCard>
+        <TeamCard title = "" name="Komal Chilukuri" 
+        headshot="" bio="Komal is a second-year Public Health major with a minor in ASL and Business. She is getting her EMT license, and in her free time she likes reading, playing guitar, and listening to music. "> </TeamCard>
         <TeamCard title = "" name="Dhruv Nistala" 
         headshot="https://i.imgur.com/9c3Xc9G.jpg" bio="Dhruv is a third-year double majoring in computer science and mathematics. He currently is a research assistant in Dell Medical School focused on using using Machine Learning to preemptively detect signs of T1 Diabetes in the Pancreas. This will be his third year in the Texas URJ. His hobbies include traveling, hiking and watching movies."> </TeamCard>
-        <TeamCard title = "" name="Yashica Sadam" 
-        headshot="https://i.imgur.com/VCnMOMM.jpg" bio="Yashica is a first-year Health & Society major who enjoys reading, listening to music, and playing with her dog. She is currently actively involved in Partners in Health Engage and Texas Brain Exercise Initiative. "> </TeamCard>
         <TeamCard title = "" name="Sriya Dommaraju" 
-        headshot="https://i.imgur.com/O5hYWUy.jpg" bio="Sriya is a second-year Biology and Plan II major who is actively involved in Biomarker Validation for Inflammatory Breast Cancer as part of the Lambowitz Lab. In her free time, Sriya enjoys playing the guitar, volleyball, and reading books."> </TeamCard>
+        headshot="https://i.imgur.com/O5hYWUy.jpg" bio="Sriya is a third-year Biology and Plan II major who is actively involved in Biomarker Validation for Inflammatory Breast Cancer as part of the Lambowitz Lab. In her free time, Sriya enjoys playing the guitar, volleyball, and reading books."> </TeamCard>
+        <TeamCard title = "" name="Marco Keller" 
+        headshot="https://i.imgur.com/D3Q4m9D.jpg" bio="Marco is a fourth-year Biology major who is actively involved in his FRI lab (Plant Pathways) and is doing research in the Johnson Lab. In his free time he plays chess, tennis, and rides trails on his bike."> </TeamCard>
+        <TeamCard title = "" name="Keshv Srinivasan" 
+        headshot="https://i.imgur.com/p4kkugE.jpg" bio="Keshv is a fourth-year Biochemistry major whose research interests are in stroke therapies and brain imaging technology. In his free time, he enjoys going on hikes, running, and watching sports."> </TeamCard>
         
         </div></Accordion.Content>
         </Accordion.Panel>
@@ -55,13 +61,14 @@ const Staff = () => {
       <AccordionContent><div className='flex flex-col items-center mb-4'>
         <TeamCard title = "" name="Aditya Venkataraman" 
         headshot="https://i.imgur.com/rYrFlHi.jpg" bio="Aditya is a first-year Biology major who researches animal behaviors and metabolism. His hobbies include bird watching, playing tennis, and watching TV shows."> </TeamCard>
-        
+        <TeamCard title = "" name="Saisha Salpekar" 
+        headshot="" bio="Saisha loves research! After law school, she wants to be a professor and teach diplomacy!"> </TeamCard>
         </div></AccordionContent></AccordionPanel>
         <AccordionPanel>
         <AccordionTitle>Assistant Directors of Communications</AccordionTitle>
         <AccordionContent><div className='flex flex-col items-center mb-4'>
         <TeamCard title = "" name="Sneha Chandak" 
-        headshot="https://i.imgur.com/rYrFlHi.jpg" bio="Sneha is a third-year Biology major who is also perusing the Scientific Computation and Data Science certificate. She is also a writer with HerCampus and involved in the FRI lab Microbe Hackers and the Havird lab. In her free time, she enjoys baking, crocheting, and other habits a grandma may enjoy."> </TeamCard>
+        headshot="" bio="Sneha is a third-year Biology major who is also perusing the Scientific Computation and Data Science certificate. She is also a writer with HerCampus and involved in the FRI lab Microbe Hackers and the Havird lab. In her free time, she enjoys baking, crocheting, and other habits a grandma may enjoy."> </TeamCard>
         
         </div></AccordionContent>
         </AccordionPanel>
