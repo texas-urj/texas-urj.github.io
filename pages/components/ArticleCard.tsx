31590b393f6b3aadb5aa59131678825065a5afc4
Meghna Vergis <meghnavergis@utexas.edu>
2024-12-03 22:44:23 -0600

    switched padding to components, not pages

--- pages/components/ArticleCard.tsx
+++ pages/components/ArticleCard.tsx
@@ -11,11 +11,12 @@ interface proppity {
     }
     const ArticleCard = ({thumbnail, title, route}: proppity) => {
           return (
+            <div className='flex justify-center mx-4'>
             <Card href= {route} className="max-w-sm mb-10" imgSrc={thumbnail}>
       <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
         {title}
       </h5>
-    </Card>
+    </Card></div>
           );
         }
     
