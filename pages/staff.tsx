435756cbe904329aa38c179415e33ee957dbc7ef
sneha-chandak <snehachandak171@gmail.com>
2025-02-25 19:46:05 -0600

    test again - sneha

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -66,7 +66,7 @@ const Staff = () => {
         <AccordionTitle>Assistant Directors of Communications</AccordionTitle>
         <AccordionContent><div className='flex flex-col items-center mb-4'>
         <TeamCard title = "" name="Sneha Chandak" 
-        headshot="" bio="Sneha is a third-year Biology major who is also perusing the Scientific Computation and Data Science certificate. She is also a writer with HerCampus and involved in the FRI lab Microbe Hackers and the Havird lab. In her free time, she enjoys baking, crocheting, and other habits a grandparent may enjoy."> </TeamCard>
+        headshot="" bio="Sneha is a third-year Biology major who is also perusing the Scientific Computation and Data Science certificate. She is also a writer with HerCampus and involved in the FRI lab Microbe Hackers and the Havird lab. In her free time, she enjoys baking, crocheting, and other habits a grandparent may enjoy. She is from Dallas."> </TeamCard>
         
         </div></AccordionContent>
         </AccordionPanel>
