651b0d624c8699b5ec4b9b4dc3ad0017bb2eaca4
sravyag24 <148296043+sravyag24@users.noreply.github.com>
2026-08-17 15:44:03 -0500

    Update Footer.tsx
    Added Samuel's email.

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -6,7 +6,7 @@ interface proppity {
     }
     const Footer = ({title}: proppity) => {
   return (
-    <div className='bg-black mb-10 mt-auto w-full'>Questions about the {title}? Email <a className='underline hover:no-underline' href='mailto:dp36455@my.utexas.edu'>dp36455@my.utexas.edu</a> with any questions or concerns.</div>
+    <div className='bg-black mb-10 mt-auto w-full'>Questions about the {title}? Email our Editors in Chiefs, Dhroov Pathare at <a className='underline hover:no-underline' href='mailto:dp36455@my.utexas.edu'>dp36455@my.utexas.edu</a> or Samuel Sliter-Hays at <a className='underline hover:no-underline' href='mailto:sms8389@my.utexas.edu'>sms8389@my.utexas.edu</a> with any questions or concerns.</div>
 
   )}
 
